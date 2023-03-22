140aa1c764a21eb35e45f4637a3713604c864854
Philip Offtermatt <p.offtermatt@gmail.com>
2023-03-22 08:32:00 +0100

    Add test

--- x/lottery/module_test.go
+++ x/lottery/module_test.go
@@ -20,17 +20,22 @@ type LotteryTestSuite struct {
 
 	coordinator *ibctesting.Coordinator
 
-	app app.App
+	lotteryApp *app.App
+	ticketApp  *app.App
 
 	// testing chains used for convenience and readability
 	lottery *ibctesting.TestChain
 	ticket  *ibctesting.TestChain
 }
 
+var apps []*app.App
+
 func (suite *LotteryTestSuite) SetupTest() {
 	suite.coordinator = ibctesting.NewCoordinator(suite.T(), 2)
 	suite.lottery = suite.coordinator.GetChain(ibctesting.GetChainID(1))
 	suite.ticket = suite.coordinator.GetChain(ibctesting.GetChainID(2))
+	suite.lotteryApp = apps[0]
+	suite.ticketApp = apps[1]
 }
 
 func NewLotteryPath(lottery, ticket *ibctesting.TestChain) *ibctesting.Path {
@@ -46,13 +51,18 @@ func NewLotteryPath(lottery, ticket *ibctesting.TestChain) *ibctesting.Path {
 func (suite *LotteryTestSuite) TestLotteryLifecycleCancel() {
 	path := NewLotteryPath(suite.ticket, suite.lottery)
 	suite.coordinator.Setup(path)
-
+ 
+	creator := suite.lottery.SenderAccount.GetAddress().String()
 	lotteryCreationMsg := types.MsgCreateLottery{
-		Creator:  suite.lottery.SenderAccount.GetAddress().String(),
+		Creator:  creator,
 		Deadline: 50,
 	}
 	_, err := suite.lottery.SendMsgs(&lotteryCreationMsg)
 	suite.Require().NoError(err)
+
+	lotteries := suite.lotteryApp.LotteryKeeper.GetAllLottery(suite.lottery.GetContext())
+	suite.Require().Equal(1, len(lotteries))
+	suite.Require().Equal(creator, lotteries[0].Creator)
 
 	bettingAmt := sdk.NewCoin("stake", sdk.NewInt(50))
 	buyer := suite.ticket.SenderAccount.GetAddress()
@@ -60,7 +70,7 @@ func (suite *LotteryTestSuite) TestLotteryLifecycleCancel() {
 	ticketContext := suite.ticket.GetContext()
 	packetTimeout := ticketContext.BlockTime().Add(50 * time.Second)
 
-	// balanceBefore := suite.ticket.GetSimApp().BankKeeper.GetBalance(suite.ticket.GetContext(), buyer, bettingAmt.Denom)
+	balanceBefore := suite.ticketApp.BankKeeper.GetBalance(suite.ticket.GetContext(), buyer, bettingAmt.Denom)
 
 	buyTicketMsg := types.MsgSendBuyTicket{
 		LotteryId:        0,
@@ -82,9 +92,10 @@ func (suite *LotteryTestSuite) TestLotteryLifecycleCancel() {
 	suite.Require().NoError(err) // relay committed
 
 	// check that on chain ticket coins were spent
-	// balanceAfter := suite.ticket.GetSimApp().BankKeeper.GetBalance(suite.ticket.GetContext(), buyer, "stake")
+	balanceAfter := suite.ticketApp.BankKeeper.GetBalance(suite.ticket.GetContext(), buyer, "stake")
 
-	// suite.Require().Equal(balanceBefore.SubAmount(bettingAmt.Amount), balanceAfter)
+	suite.Require().True(balanceBefore.SubAmount(bettingAmt.Amount).IsEqual(balanceAfter))
+
 }
 
 func TestTransferTestSuite(t *testing.T) {
@@ -96,6 +107,8 @@ func SetupTestingApp() (ibctesting.TestingApp, map[string]json.RawMessage) {
 
 	encCdc := app.MakeEncodingConfig()
 	application := app.New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, simapp.DefaultNodeHome, 5, encCdc, simapp.EmptyAppOptions{})
+	apps = append(apps, application)
+
 	return application, app.NewDefaultGenesisState(encCdc.Marshaler)
 }
 
